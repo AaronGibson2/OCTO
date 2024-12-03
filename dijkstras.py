5d9042f160733e098b3571c2ff9a14c0c26ae98d
Juan  Castaneda <jcc@Mac.attlocal.net>
2024-12-02 18:05:16 -0600

    Working dijkstras with simple visualization)

--- dijkstras.py
+++ dijkstras.py
@@ -1,14 +1,81 @@
 # I M P O R T S   &   D E P E N D E N C I E S ----------------------
 import heapq
-from typing import Dict, List, Set, Tuple
+from typing import Dict, List, Set, Tuple, Optional, Mapping
 
 
 # F U N C T I O N S ------------------------------------------------
 
-def dijkstra(graph, start_node: int, end_node: int) -> Tuple[Dict[int, float], Dict[int,int]]:
+# Dijkstras Algorithm Implementation
+def dijkstra(graph, start_node: int, end_node: int) -> Tuple[Dict[int, float], Dict[int,Optional[int]]]:
 
     # Set all unknown node distances to infinity
     distances = {node: float('infinity') for node in graph.nodes()}
 
     # Set distance from starting_node to starting_node to 0
     distances[start_node] = 0;
+
+    # Dictionary to store previous nodes for optimal path
+    previous: Dict[int, Optional[int]] = {node: None for node in graph.nodes()}
+
+    # Priority queu to store distance to node pairs as we discover
+    # Add one item start_node to start_node distance of 0
+    pq = [(0, start_node)]
+
+    # Set to keep track of those visited
+    visited: Set[int] = set()
+
+    # While my PQ is not empty
+    while pq:
+
+        # Unpack from pq using heapop()
+        current_distance, current_node = heapq.heappop(pq)
+
+        # Check if we are at end node
+        if current_node == end_node:
+            break
+
+        # Check if the node we are at has been visited
+        if current_node in visited:
+            continue    # Skip if it has been visited
+        # Add current_node to those visited
+        visited.add(current_node)
+
+        # Check all neighbors of the current node
+        for neighbor in graph.neighbors(current_node):
+            # If we have already seen neighbor then skip
+            if neighbor in visited:
+                continue
+
+            # Get edge data from current to neighbor
+            edge_data = graph.get_edge_data(current_node, neighbor)
+
+            # Get the first edge and get length attribute
+            edge_length = edge_data[0]['length']
+
+            distance = current_distance + edge_length
+
+            # Perform Relaxation
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                previous[neighbor] = current_node
+
+                # Push new edge into pq
+                heapq.heappush(pq, (distance, neighbor))
+
+
+    return distances, previous
+
+# Reconstructs path from start -> end using previous dict
+def reconstruct_path(previous: Mapping[int, Optional[int]], start_node: int, end_node: int) -> List[int]:
+
+    # Initialize list path
+    path = []
+    current_node = end_node
+
+    # Traverse previous dict to get optimal path
+    while current_node is not None:
+        path.append(current_node)
+        current_node = previous[current_node]
+
+    # Return reversed path to get start to end
+    return path[::-1]
--- main.py
+++ main.py
@@ -3,18 +3,43 @@ import matplotlib.pyplot as plt
 import networkx
 import osmnx as ox
 
+from dijkstras import dijkstra, reconstruct_path
+
 # M A P   S E T U P ------------------------------------------------
 
 # Location and map settings
 location = "Orange County, Florida, USA"
 graph = ox.graph_from_place(location, network_type='drive')
 
+# Get data points for verification
+num_nodes = len(graph.nodes())
+num_edges = len(graph.edges())
+print(f"Number of nodes: {num_nodes}")
+print(f"Number of edges: {num_edges}")
 
-# Visualization set up
-fig, ax = ox.plot_graph(
+# Use two random nodes for TESTING ------------
+nodes = list(graph.nodes())
+
+start_node = nodes[0]
+end_node = nodes[100]
+
+# Run our implementation
+distances, previous = dijkstra(graph, start_node, end_node)
+
+# Get the shortest path
+path = reconstruct_path(previous, start_node, end_node)
+
+
+print(f"Total distance: {distances[end_node]:.2f} meters")
+
+# Set up visualization with that path
+fig, ax = ox.plot_graph_route(
     graph,
+    path,
+    route_color='red',
+    route_linewidth=6,
+    route_alpha=0.5,
     node_size=0,
-    node_color='#66cc66',
     edge_color='blue',
     edge_linewidth=1,
     edge_alpha=0.5,
@@ -23,12 +48,7 @@ fig, ax = ox.plot_graph(
 
 plt.show()
 
-# Get data points
-num_nodes = len(graph.nodes())
-num_edges = len(graph.edges())
 
-print(f"Number of nodes: {num_nodes}")
-print(f"Number of edges: {num_edges}")
 
 # Save the graph to a file for later reuse
 ox.save_graphml(graph, filepath="data/gainesville.graphml")
